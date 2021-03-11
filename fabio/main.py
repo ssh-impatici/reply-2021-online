39fb583ba08eb3097e84da2fbb91a8e5fdc2eb8d
Fabio <fabio.sangregorio@gmail.com>
2021-03-11 17:15:49 +0100

    Update lorenzo

--- fabio/main.py
+++ fabio/main.py
@@ -5,12 +5,12 @@ from solver import solver
 if __name__ == '__main__':
 
     tasks = [
-        'a',
-        'b',
-        'c',
-        'd',
-        'e',
-        'f'
+        'data_scenarios_a_example.in',
+        'data_scenarios_b_mumbai.in',
+        'data_scenarios_c_metropolis.in',
+        'data_scenarios_d_polynesia.in',
+        'data_scenarios_e_sanfrancisco.in',
+        'data_scenarios_f_tokyo.in'
     ]
 
     for task in tasks:
