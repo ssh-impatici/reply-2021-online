931889f35d69b0851f817b02b46fae15c84afa1d
Samuele Ferri <samuelexferri@gmail.com>
2021-03-11 17:51:32 +0100

    Analysis

--- samuele/solver.py
+++ samuele/solver.py
@@ -1,5 +1,6 @@
 import time
 
+from analysis import analysis
 from classes import *
 from judge import judge
 from read import read
@@ -9,14 +10,21 @@ path = './input/'
 
 
 def solver(task):
-    start = time.time()
 
-    _ = read(path + task + '.txt')
+    start = time.time()
 
     output = []
 
-    write('./output/' + task + '.txt', output)
+    buildings, antennas, reward = read(path + task)
 
-    score = judge(output)
+    # Analysis
+    analysis(task, buildings, antennas, reward)
+
+    for i in range(min(len(buildings), len(antennas))):
+        antennas[i].x = buildings[i].x
+        antennas[i].y = buildings[i].y
+        output.append(antennas[i])
+
+    write('output/' + str(task.split('.')[0]) + '.txt', output)
 
     print(task, str(time.time() - start))
